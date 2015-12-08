daa95fde1b4edba9ab5e86052278f5e986ec2033
ChrisLane <ChrisLane@users.noreply.github.com>
2015-12-08 01:04:30 +0000

    Small formatting and stop matlab warnings

--- MATLAB/functions/fap.m
+++ MATLAB/functions/fap.m
@@ -1,6 +1,6 @@
-function x = fap(img, Filter)
+function x = fap(img, filter)
 %FAP applies 2 given X and Y filters to an image given as input,
 %calculates the magnitude of change from the 2
-x = conv2(img, Filter, 'same');
+x = conv2(double(img), double(filter), 'same');
 
 end
