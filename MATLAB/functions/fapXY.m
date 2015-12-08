547926f04c81bffc2426d65c4db452db544ec1b2
ChrisLane <ChrisLane@users.noreply.github.com>
2015-12-08 00:54:08 +0000

    Spelling fix

--- MATLAB/functions/fapXY.m
+++ MATLAB/functions/fapXY.m
@@ -1,8 +1,9 @@
-function c = fapXY(img, xFilter, yFilter, treshold)
+function c = fapXY(img, xFilter, yFilter, threshold)
 %FAP applies 2 given X and Y filters to an image given as input,
 %calculates the magnitude of change from the 2
 x = conv2(img, xFilter, 'same');
 y = conv2(img, yFilter, 'same');
 c = magnitude(x, y, treshold);
+c = magnitude(x, y, threshold);
 
 end
