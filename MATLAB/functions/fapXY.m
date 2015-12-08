d18b410f8acf8b883c897dae7d9a27fa594da307
ChrisLane <ChrisLane@users.noreply.github.com>
2015-12-08 00:54:29 +0000

    Stop matlab complaining about obsolete types

--- MATLAB/functions/fapXY.m
+++ MATLAB/functions/fapXY.m
@@ -1,9 +1,8 @@
 function c = fapXY(img, xFilter, yFilter, threshold)
 %FAP applies 2 given X and Y filters to an image given as input,
 %calculates the magnitude of change from the 2
-x = conv2(img, xFilter, 'same');
-y = conv2(img, yFilter, 'same');
-c = magnitude(x, y, treshold);
+x = conv2(double(img), double(xFilter), 'same');
+y = conv2(double(img), double(yFilter), 'same');
 c = magnitude(x, y, threshold);
 
 end
