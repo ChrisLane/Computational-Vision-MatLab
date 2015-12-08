2a8b06bac9555bd4a135c678712c650b3ac1c43e
ChrisLane <ChrisLane@users.noreply.github.com>
2015-12-08 00:53:33 +0000

    Formatting on magnitude

--- MATLAB/functions/magnitude.m
+++ MATLAB/functions/magnitude.m
@@ -1,4 +1,4 @@
-function m = magnitude(x,y,t)
+function m = magnitude(x, y, t)
 %MAGN Calculates the magnitude of change from 2 gradients given as
 %input and applies a gradient to it
 m = sqrt(x.^2 + y.^2) > t;
