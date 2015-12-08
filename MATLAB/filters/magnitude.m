993ab9a4227b1c120486cbe3a6d1403f2d8d31ed
Matthias Casula <matthiascasula@gmail.com>
2015-12-08 00:34:13 +0000

    Function to apply functions to image (fap)

--- MATLAB/filters/magnitude.m
+++ MATLAB/filters/magnitude.m
@@ -1,8 +1,8 @@
-function m = magnitude( x,y )
+function m = magnitude(x,y)
 %MAGNITUDE Calculates the magnitude of change from 2 gradients given as
 %input
 m = sqrt(x.^2 + y.^2); 
-show_image(m>15)
+show_image(m > 15)
 
 end
 
