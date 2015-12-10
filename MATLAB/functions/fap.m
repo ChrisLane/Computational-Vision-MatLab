2e514acb2ba5b72c9ba2bf042bbbafe1d8411f95
ChrisLane <ChrisLane@users.noreply.github.com>
2015-12-10 16:06:15 +0000

    No need to cast to double, already doubles now

--- MATLAB/functions/fap.m
+++ MATLAB/functions/fap.m
@@ -1,6 +1,6 @@
 function x = fap(img, filter)
 %FAP applies 2 given X and Y filters to an image given as input,
 %calculates the magnitude of change from the 2
-x = conv2(double(img), double(filter), 'same');
+x = conv2(img, filter, 'same');
 
 end
